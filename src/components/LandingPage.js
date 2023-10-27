faf64e9e6c7725fa9483819951e4ceca4dd200bf
arvindsingh-cedcommerce <arvindsingh@cedcoss.com>
2023-10-27 13:02:11 +0530

    second commit in github-demo

---
+++ src/components/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <section className="section">
+      <p>Welcome to the Landing Page! This is a sample landing page.</p>
+    </section>
+  );
+}
+
+export default LandingPage
